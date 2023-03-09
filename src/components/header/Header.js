694bb639e8e420957df02639884e82db1ed90504
Pravendra Kumar <pravendraksj7.com>
2023-03-09 11:57:19 +0530

    upadted

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,12 +5,14 @@ import { Link } from "react-router-dom";
 function Header(){
     return(
         <div className="header">
-          <div className="headerLeft">
+          <div className="header-left">
+               <Link className="App-link"to="/" ><img className="header-icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png"/></Link>
           </div>
-            <Link className="App-link"to="/" ><img className="header-icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png"/></Link>
-            <Link className="App-link"to="/movies/popular" ><span>Popular</span></Link>
-            <Link className="App-link"to="/movies/top_rated" ><span>Top Rated</span></Link>
-            <Link className="App-link"to="/movies/upcoming" ><span>Upcoming</span></Link>
+            <div className="header-right">
+               <Link className="App-link"to="/movies/popular" ><span>Popular</span></Link>
+               <Link className="App-link"to="/movies/top_rated" ><span>Top Rated</span></Link>
+               <Link className="App-link"to="/movies/upcoming" ><span>Upcoming</span></Link>
+            </div>
         </div>
     )
 }
